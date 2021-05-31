c11da9470fca0760922459e275a3f0f26a910bcd
sourav mazumdar <sourav.mazumdar@bell.ca>
2021-05-31 18:04:23 +0530

    Initial Commit

---
+++ Contants/NameConst.swift
@@ -0,0 +1,15 @@
+//
+//  NameConst.swift
+//  Contants
+//
+//  Created by Sourav Mazumdar on 28/05/21.
+//
+
+import Foundation
+
+public class NameConst {
+    public init() { }
+    public func getTitle() -> String {
+        return "Testing"
+    }
+}
